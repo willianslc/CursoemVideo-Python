65b0b7a2e31d17d6990bf7eff47172f425d8b49a
willianslc <willianslc94@gmail.com>
2026-08-05 01:11:10 -0300

    Fazendo alguns adendos.

--- ex001.py
+++ ex001.py
@@ -1,2 +1,17 @@
 nome = input("Qual o seu nome? \n") #O \n ali faz com que o nome que for digitado fique na linha de baixo
 print("Prazer em conhecer vc", nome)
+
+"""
+CÓDIGO DO PROFESSOR
+
+nome = input("Qual o seu nome? \n")
+print("É um prazer te conhecer, {}!" .format(nome))
+
+O resultado desse código fica assim:
+
+É um prazer te conhecer, Fulano!
+
+as chaves acompanhado do parenteses indica que o sinal de exclamação
+deve ficar no final da nome da pessoa e quem fará isso é o .format.
+Ele é responsável por preencher a lacuna que foi criado com as {} dentro das aspas.
+"""
