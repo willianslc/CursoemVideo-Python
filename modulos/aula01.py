d5b5a0368cf89de6828e45d4f07170bffa53d72a
willianslc <willianslc94@gmail.com>
2026-08-20 21:44:36 -0300

    Organizando as coisas e fazendo anotações das aulas.

--- modulos/aula01.py
+++ modulos/aula01.py
@@ -1,23 +1,11 @@
-"""
-from math import sqrt, ceil ----> Isso aqui é o que chama de modulo\biblioteca
-import math ----> Outra forma de chamar a biblioteca mas assim vem a biblioteca toda
-num = int(input("Digite um número: "))
-raiz = sqrt(num)
+import random #Importa a biblioteca toda
 
-print("A raiz de {} é {:.2f}".format(num, ceil(raiz)))
+from math import sqrt, ceil, floor #Importa só o que vai usar
+num1 = int(input("Digite um número: "))
+raiz = sqrt(num1) #SQRT faz raíz quadrada nesse caso
 
-Math é um biblioteca com algumas ferramentas matemáticas, seja para arredondar resultados para mais ou para menos
-raiz quadrada, no caso sqrt é a raiz quadrada, ceil arredonda para cima, floor para baixo
-"""
-"""
-import random #Importamos a biblioteca random, ela permite que a máquina gere números aleatórios
-num = random.randint(1, 10) #Aqui pedimos para ele gerar um número inteiro aleatório de 1 a 10
-print(num)
+num2 = random.random() #Gera número aleatório entre 0 e 1
+print(num2)
 
-Se colocar da seguinte forma...
-num = random.random() 
-print(num)
-
-Ele iria retornar um número qualquer que ele iria pegar na memória da máquina, porém dessa forma ele só gera
-numeros aleatório entre 0 e 1
-"""
+num3 = random.randint(1, 10) #Pega um número "inteiro" aleatório entre 1 e 10
+print(num3)
